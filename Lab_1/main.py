425392c0e09c781808e4816aae8fe0d0be9274b1
gertyboy1200 <garrett.kolenbrander@gmail.com>
2025-01-22 13:09:53 -0700

    initial commit🚧
    remade repo with proper wording and naming for full credit

---
+++ Lab_1/main.py
@@ -0,0 +1,49 @@
+"""
+    StdIO Lab
+    ASCII Art - using literals and variables
+    
+    Updated By: Garrett Kolenbrander #FIXED
+    Date: 21/1/25 #FIXED
+    
+    This program produces an ASCII art on the console.
+
+    Algorithm steps: 
+    1. Use variables to store data/values
+    2. Write a series of print statements to print the data/values to the console
+"""
+
+print("Welcome to ASCII Art Program...\n")
+
+username: str = input("What is your name? ") #FIXED
+print("Nice meeting you, ", username, "!\n") #FIXED
+# must output: Nice meeting you, <name>!
+
+# prompt user to enter the semester and store the value into semester variable using input function
+semester: str = input("What semester is this [Fall/Spring]? ")
+print("This is " + semester + " semester.\n")
+
+year: str = input("Enter the year: ") #FIXED
+print("This is", year, "year.\n") #FIXED
+# must output: This is <year> year.
+
+print("Hope you like my ASCII art...\n\n")
+
+
+
+
+
+
+
+line1: str = "  |\\_/|   **********************    (\\_/)"
+print(line1)
+line2: str = " / @ @ \  *      ASCII Lab     *   (='.'=)"
+print(line2) #FIXED7
+print('( > 0 < ) *      Garrett       * ( " )_( " )') #FIXED8
+line4: str  = '  >>x<<   *      CSCI 110      *'
+print(line4) #FIXED9
+line5: str = ' /  O  \  **********************' #FIXED10
+print(line5)
+
+# Note: You can add more lines or print more ASCII arts of your choice if you'd like...
+
+print("\nGood bye...  \n")
